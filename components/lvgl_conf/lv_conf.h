3a006e1b85348defb2ce8e281201484d1aae2644
Long Nguyen <long.nguyenviet@alibaba-inc.com>
2025-08-29 11:55:21 +0700

    Update

---
+++ components/lvgl_conf/lv_conf.h
@@ -0,0 +1,50 @@
+//
+// Created by long.nguyenviet on 8/6/25.
+//
+
+#ifndef LV_CONF_H
+#define LV_CONF_H
+/*==================
+   COLOR SETTINGS
+ *==================*/
+
+#define LV_COLOR_DEPTH 1
+#define LV_COLOR_16_SWAP 0
+#define LV_DRAW_SW_COMPLEMENT_BASIC 1
+
+/*==================
+   DISPLAY SETTINGS
+ *==================*/
+
+#define LV_HOR_RES_MAX          480
+#define LV_VER_RES_MAX          800
+
+/*==================
+   MEMORY SETTINGS
+ *==================*/
+
+#define LV_MEM_CUSTOM 0
+
+/*==================
+   MISC SETTINGS
+ *==================*/
+
+#define LV_USE_LOG 1
+
+/*==================
+   FEATURE SETTINGS
+ *==================*/
+
+#define LV_USE_LABEL 1
+#define LV_LABEL_TEXT_SELECTION 0
+#define LV_LABEL_LONG_TXT_HINT 1
+
+/*==================
+   PORTING
+ *==================*/
+
+#define LV_TICK_CUSTOM 0
+
+
+
+#endif //LV_CONF_H
